e73d257ba1e4429a9487c5abaf27c9b5f9b4df39
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-07-21 16:55:24 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nearby4b::Application.config.secret_token = 'c04124033d3ede5c191f564f1aacc8df5b3d0b1e22abe8a2025d457054ddb1847329b81942da1851773b9cfbb399377c8e68e0dfa35d2f7d65e29f3f344ba162'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Nearby4b::Application.config.session_store :cookie_store, key: '_nearby4b_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Nearby4b::Application.config.session_store :active_record_store
