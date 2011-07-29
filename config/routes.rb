a820669a40025a4a34f38ddfc6b158ca416cb8b8
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-07-29 08:36:00 +0200

    signup and signin done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Nearby4b::Application.routes.draw do
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "communities#index"
+  resources :users
+  resources :sessions
+    
   resources :locations
 
   resources :people
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110726054254) do
+ActiveRecord::Schema.define(:version => 20110729055904) do
 
   create_table "communities", :force => true do |t|
     t.string   "name"
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(:version => 20110726054254) do
     t.boolean  "contact"
     t.boolean  "planner"
     t.boolean  "community_sponsor"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
